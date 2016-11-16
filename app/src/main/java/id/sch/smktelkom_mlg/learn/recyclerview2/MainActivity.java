99929424c3155273134d39ec73461fbd6724ca56
fadilghulam <razerstarsdota@gmail.com>
2016-11-16 23:59:43 +0700

    RecyclerView Grid

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/MainActivity.java
@@ -2,13 +2,10 @@ package id.sch.smktelkom_mlg.learn.recyclerview2;
 
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -16,22 +13,27 @@ import java.util.ArrayList;
 import id.sch.smktelkom_mlg.learn.recyclerview2.adapter.HotelAdapter;
 import id.sch.smktelkom_mlg.learn.recyclerview2.model.Hotel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+
+{
 
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
         mAdapter = new HotelAdapter(mList);
         recyclerView.setAdapter(mAdapter);
 
         fillData();
+
     }
 
     private void fillData() {
@@ -40,12 +42,9 @@ public class MainActivity extends AppCompatActivity {
         String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
         TypedArray a = resources.obtainTypedArray(R.array.places_picture);
         Drawable[] arFoto = new Drawable[a.length()];
+
         for (int i = 0; i < arFoto.length; i++) {
-            BitmapDrawable bd = (BitmapDrawable) a.getDrawable(i);
-            RoundedBitmapDrawable rbd =
-                    RoundedBitmapDrawableFactory.create(getResources(), bd.getBitmap());
-            rbd.setCircular(true);
-            arFoto[i] = rbd;
+            arFoto[i] = a.getDrawable(i);
         }
         a.recycle();
 
